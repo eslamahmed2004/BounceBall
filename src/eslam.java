1674547c3451e3585e7d2191b960721521060ea0
eslamahmed2004 <ea377313@gmail.com>
2024-12-14 18:45:15 +0200

    ea

--- src/eslam.java
+++ src/eslam.java
@@ -2,6 +2,9 @@ public class eslam {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
     }
 
 
