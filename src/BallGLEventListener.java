abc163f38d10621fbcacc0f5f044e5251858a68f
Marwan-Essam55 <marawan.elbob@gmail.com>
2024-12-15 02:13:40 -1000

    add

--- src/BallGLEventListener.java
+++ src/BallGLEventListener.java
@@ -4,10 +4,6 @@ import javax.media.opengl.GL;
 import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLEventListener;
 import javax.media.opengl.glu.GLU;
-import javax.media.opengl.GL;
-import javax.media.opengl.GLAutoDrawable;
-import javax.media.opengl.GLEventListener;
-import javax.media.opengl.glu.GLU;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import java.awt.event.KeyEvent;
@@ -19,19 +15,15 @@ import java.io.IOException;
 
 class BallGLEventListener implements GLEventListener, KeyListener, MouseListener {
 
-    File file = new File("C:\\BounceBall\\src\\PNG");
+    File file = new File("D:\\CS304\\src\\PNG\\");
     String[] textureNames = file.list();
     TextureReader.Texture[] texture = new TextureReader.Texture[textureNames.length];
     int[] textures = new int[textureNames.length];
 
     public static void main(String[] args) {
         new BounceBall();
-        BounceBall.animator.start();
     }
-    /*
-     5 means gun in array pos
-     x and y coordinate for gun
-     */
+
     double width = 700, hight = 500;
     GL gl;
 
@@ -42,11 +34,12 @@ class BallGLEventListener implements GLEventListener, KeyListener, MouseListener
         gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         gl.glEnable(GL.GL_TEXTURE_2D);
         gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
+
         gl.glGenTextures(textureNames.length, textures, 0);
 
         for (int i = 0; i < textureNames.length; i++) {
             try {
-                texture[i] = TextureReader.readTexture("C:\\BounceBall\\src\\PNG\\" + textureNames[i], true);
+                texture[i] = TextureReader.readTexture("D:\\CS304\\src\\PNG\\" + textureNames[i], true);
                 gl.glBindTexture(GL.GL_TEXTURE_2D, textures[i]);
                 glu.gluBuild2DMipmaps(
                         GL.GL_TEXTURE_2D,
@@ -66,94 +59,87 @@ class BallGLEventListener implements GLEventListener, KeyListener, MouseListener
         gl.glLoadIdentity();
         glu.gluOrtho2D(-width, width, -hight, hight);
         gl.glMatrixMode(GL.GL_MODELVIEW);
-
     }
 
-    double x ;
-    double x_ball = 0 , y_ball = -100 ;
-    double dx = -5 ;
-     double dy = 5 ;
-    boolean startBall = false ;
+    double x;
+    double x_ball = 0, y_ball = -400;
+    double dx = -5;
+    double dy = 5;
+    boolean startBall = false;
+
+    long startTime = System.nanoTime();
+    long elapsedTime;
+
     public void display(GLAutoDrawable gld) {
         gl = gld.getGL();
         gl.glClear(GL.GL_COLOR_BUFFER_BIT);
         gl.glLoadIdentity();
 
+        // رسم الخلفية
         DrawBackground(70);
-        for (int i = -100; i < 100; i += 45) {
+
+        // رسم الكائنات الأخرى
+        for (int i = -400; i < 400; i += 45) {
             DrawSprite(i, 0, 3, 20, 10);
         }
+
+        // رسم القلوب
+        DrawSprite(-390, 430, 60, 50, 50);
+        DrawSprite(-510, 430, 60, 50, 50);
+        DrawSprite(-630, 430, 60, 50, 50);
+
+        // رسم الكائنات المتبقية
         gl.glPushMatrix();
         gl.glTranslated(x, -450, 0);
         DrawSprite(0, -0, 50, 100, 15);
         gl.glPopMatrix();
 
+        // رسم الكرة المتحركة
         gl.glPushMatrix();
-        gl.glTranslated(x_ball, y_ball , 0);
+        gl.glTranslated(x_ball, y_ball, 0);
         DrawSprite(0, 0, 58, 15, 15);
         gl.glPopMatrix();
 
-        if (startBall){
+        if (startBall) {
             x_ball += dx;
             y_ball += dy;
         }
-        if (-hight >= y_ball){
-            startBall = false ;
-            x_ball = 0  ;
-            y_ball = -100 ;
+
+        if (-hight >= y_ball) {
+            startBall = false;
+            x_ball = 0;
+            y_ball = -400;
         }
 
-        double r = 15 ;
-
-        if(x_ball >=  width - r || x_ball <= -width + r) {
+        // إدارة الاصطدامات
+        double r = 15;
+        if (x_ball >= width - r || x_ball <= -width + r) {
             dx = -dx;
-
-                playSound();
-
+            playSound();
         }
-        if(y_ball >=  hight - r  ) {
+        if (y_ball >= hight - r) {
             dy = -dy;
-
-                playSound();
-
+            playSound();
         }
-        if ( y_ball <= -hight + r){
-            x_ball = 0 ;
-            y_ball = -100 ;
-            x = 0 ;
-            startBall = false ;
-            dx = 3 ;
-            dy = 3 ;
-        }
-        // Bar dimensions and position
-        double barWidth = 200;
-        double barHeight = 15;
-        double barY = -450; // Bar's fixed Y position
-
-// Check collision with the bar
-        if (y_ball - r <= barY + barHeight / 2 && y_ball + r >= barY - barHeight / 2) {
-            if (x_ball >= x - barWidth / 2 && x_ball <= x + barWidth / 2) {
-                dy = -dy; // Reverse vertical direction
-                double barWight = 200 ;
-
-                dx = (x_ball-x)/(100/3) ;
-
-                playSound();
-            }
+        if (y_ball <= -hight + r) {
+            x_ball = 0;
+            y_ball = -400;
+            x = 0;
+            startBall = false;
+            dx = 3;
+            dy = 3;
         }
 
+        elapsedTime = System.nanoTime() - startTime;
+        long timeLeft = 6_000_000_000L - elapsedTime;
 
-
-        DrawSprite(-650, 450, 71, 30, 30);
+//        displayTime(gl, timeLeft);
     }
 
-    public double sqrdDistance(double x, double y, double x1, double y1){
-        return Math.pow(x-x1,2)+Math.pow(y-y1,2);
-    }
-
+    // دالة لتشغيل الصوت
     public static void playSound() {
         try {
-            File wavFile = new File("C:\\BounceBall\\src\\solid.wav");
+            File wavFile = new File("D:\\CS304\\src\\solid.wav");
             Clip clip = AudioSystem.getClip();
             clip.open(AudioSystem.getAudioInputStream(wavFile));
             clip.start();
@@ -162,17 +148,14 @@ class BallGLEventListener implements GLEventListener, KeyListener, MouseListener
         }
     }
 
-
-
-
+    // دالة رسم الصورة
     public void DrawSprite(double x, double y, int index, float scale_x, float scale_y) {
         gl.glEnable(GL.GL_BLEND);
         gl.glBindTexture(GL.GL_TEXTURE_2D, textures[index]);
 
         gl.glPushMatrix();
-        // Adjust translation to fit the new orthographic projection
         gl.glTranslated(x, y, 0);
-        gl.glScaled(scale_x, scale_y, 1); // Adjusted scaling factor
+        gl.glScaled(scale_x, scale_y, 1);
         gl.glBegin(GL.GL_QUADS);
         gl.glTexCoord2f(0.0f, 0.0f);
         gl.glVertex3f(-1.0f, -1.0f, -1.0f);
@@ -188,7 +171,7 @@ class BallGLEventListener implements GLEventListener, KeyListener, MouseListener
         gl.glDisable(GL.GL_BLEND);
     }
 
-
+    // رسم الخلفية
     public void DrawBackground(int index) {
         gl.glEnable(GL.GL_BLEND);
         gl.glBindTexture(GL.GL_TEXTURE_2D, textures[index]);
@@ -210,24 +193,16 @@ class BallGLEventListener implements GLEventListener, KeyListener, MouseListener
         gl.glDisable(GL.GL_BLEND);
     }
 
-
-
-
-    /*
-     * KeyListener 8432677
-     */
-
-
+    // KeyListener
     @Override
     public void keyPressed(final KeyEvent event) {
         System.out.println("key pressed");
-//        System.out.println(keyCode);
         int keycode = event.getKeyCode();
         System.out.println(x);
 
-        if(x >= -width+60 && keycode ==KeyEvent.VK_LEFT ) x -= 10;
-        if(x <=  width-60 && keycode ==KeyEvent.VK_RIGHT ) x += 10;
-        if(keycode ==KeyEvent.VK_SPACE ) startBall = true ;
+        if (x >= -width + 60 && keycode == KeyEvent.VK_LEFT) x -= 40;
+        if (x <= width - 60 && keycode == KeyEvent.VK_RIGHT) x += 40;
+        if (keycode == KeyEvent.VK_SPACE) startBall = true;
     }
 
     @Override
@@ -236,12 +211,9 @@ class BallGLEventListener implements GLEventListener, KeyListener, MouseListener
     }
 
     @Override
-
     public void keyTyped(final KeyEvent event) {
         System.out.println("key typed ");
-        // don't care
     }
-
 
     public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
     }
@@ -252,20 +224,16 @@ class BallGLEventListener implements GLEventListener, KeyListener, MouseListener
     @Override
     public void mouseClicked(MouseEvent e) {
         System.out.println("you click");
-
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
-        System.out.println("presed");
-
+        System.out.println("pressed");
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
         System.out.println("released");
-
-
     }
 
     @Override
@@ -278,13 +246,24 @@ class BallGLEventListener implements GLEventListener, KeyListener, MouseListener
         System.out.println("exited");
     }
 
-    private double convertX(double x, double width) {
+    // دالة لعرض الوقت المتبقي
+    public void displayTime(GL gl, long timeleft) {
+        int secondleft = (int) (timeleft / 1_000_000_000L)+1;
+        String timeString = String.valueOf(secondleft);
 
-        return x - this.width;
+        // عرض كل رقم من الوقت المتبقي
+        for (int i = 0; i < timeString.length(); i++) {
+            char digit = timeString.charAt(i); // الحصول على الرقم كحرف
+            int digitIndex = digit - '0' + 73; // حساب المؤشر المناسب للرقم
+
+            // التأكد من أن digitIndex ضمن النطاق المسموح به
+            if (digitIndex >= textures.length+2) {
+                digitIndex = 73; // إعادة التعيين إلى أول رقم
+            }
+
+            // عرض الرقم في المكان المناسب
+            DrawSprite(-100 + (i * 30), 400, digitIndex, 20, 20);
+
+        }
     }
-
-    private double convertY(double y, double height) {
-        return hight - y;
-    }
-
 }
--- src/BounceBall.java
+++ src/BounceBall.java
@@ -1,50 +1,99 @@
+import com.sun.opengl.util.FPSAnimator;
 
-import com.sun.opengl.util.FPSAnimator ;
-
-import java.awt.*;
-import javax.swing.*;
 import javax.media.opengl.GLCanvas;
-
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import com.sun.opengl.util.Animator;
+import com.sun.opengl.util.FPSAnimator;
 
 public class BounceBall extends JFrame {
-    static FPSAnimator animator  ;
-    GLCanvas glcanvas  = new GLCanvas();
-    final private BallGLEventListener listener = new BallGLEventListener();
+    public static FPSAnimator animator;
+    private GLCanvas glcanvas = new GLCanvas();
+    private final BallGLEventListener listener = new BallGLEventListener();
+    private int timeLeft = 60;
+    private JLabel timerLabel = new JLabel("Time left: 60", SwingConstants.CENTER);
+    private JButton startStopButton = new JButton("Start Game");
+    private Timer countdownTimer;
+
+
+
 
     public BounceBall() {
         super("Bounce Ball");
-        animator = new FPSAnimator(glcanvas , 60) ;
+        animator = new FPSAnimator(glcanvas, 60);
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // إعداد الغcanvas ومراقبي الأحداث
         glcanvas.addGLEventListener(listener);
         glcanvas.addKeyListener(listener);
         glcanvas.addMouseListener(listener);
 
-        add(glcanvas, BorderLayout.CENTER);
-        setSize(1400, 1000);
-        glcanvas.setFocusable(true);
-        centerWindow();
-        setLocationRelativeTo(this);
-        setVisible(true);
+        // إعداد المؤقت النصي
+        timerLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        timerLabel.setForeground(Color.RED);
 
+        // إعداد زر التحكم في اللعبة
+        startStopButton.addActionListener(this::toggleGame);
+
+        // إعداد المؤقت
+        setupCountdownTimer();
+
+        // ترتيب المكونات
+        JPanel controlPanel = new JPanel();
+        controlPanel.add(startStopButton);
+
+        add(glcanvas, BorderLayout.CENTER);
+        add(timerLabel, BorderLayout.NORTH);
+        add(controlPanel, BorderLayout.SOUTH);
+
+        setSize(1400, 1000);
+        centerWindow();
+        setVisible(true);
+        glcanvas.setFocusable(true);
+        glcanvas.requestFocus();
     }
 
+    private void toggleGame(ActionEvent e) {
+        if (!animator.isAnimating()) {
+            animator.start();
+            countdownTimer.start();
+            startStopButton.setText("Stop Game");
+        } else {
+            animator.stop();
+            countdownTimer.stop();
+            startStopButton.setText("Start Game");
+        }
+    }
 
+    private void setupCountdownTimer() {
+        countdownTimer = new Timer(1000, e -> {
+            timeLeft--;
+            timerLabel.setText("Time left: " + timeLeft);
+
+            if (timeLeft <= 0) {
+                countdownTimer.stop();
+                animator.stop();
+                JOptionPane.showMessageDialog(this, "Time's up!", "Game Over", JOptionPane.INFORMATION_MESSAGE);
+                startStopButton.setText("Start Game");
+            }
+        });
+    }
 
     public void centerWindow() {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        Dimension frameSize  = this.getSize();
+        Dimension frameSize = this.getSize();
 
-
-        if (frameSize.width  > screenSize.width ) frameSize.width  = screenSize.width;
+        if (frameSize.width > screenSize.width) frameSize.width = screenSize.width;
         if (frameSize.height > screenSize.height) frameSize.height = screenSize.height;
 
-
-        this.setLocation (
-                (screenSize.width  - frameSize.width ) >> 1,
+        this.setLocation(
+                (screenSize.width - frameSize.width) >> 1,
                 (screenSize.height - frameSize.height) >> 1
         );
     }
 }
-
-
-
